569c7b2535f0f8e80921053077145aae7b50a7ad
sri-raguram <132447092+sri-raguram@users.noreply.github.com>
2023-05-30 18:14:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# The General GET command processing
+@app.route('/', methods = ['GET'])
+def index():
+    return("\n=====================\nWelcome to BRTGPT\nCONGRATULATIONS you have SUCCESSFULLY CONNECTED\n=====================\n\n")
+
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
