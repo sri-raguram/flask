1f04ee82f70634de020c7368aeb07ccc8f7544d5
sri-raguram <132447092+sri-raguram@users.noreply.github.com>
2023-05-30 17:57:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -191,7 +191,7 @@ def postJsonHandler():
 
 ##############################
 # RUN the FLUSK program in debugging mode#
-app.run(port=8000,debug=True)
+app.run(host='0.0.0.0',port=8000,debug=True)
 
 #########################################################################
 
